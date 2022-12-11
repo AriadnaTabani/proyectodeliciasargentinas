4c74f40906d7294243504006681b130e45f7928f
AriadnaTabani <117049454+AriadnaTabani@users.noreply.github.com>
2022-12-10 23:31:32 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function cambiarfondo () { document.body.style.backgroundImage = "none";
+                         document.body.style.backgroundColor = "#1A144C";
+                         document.body.style.color = "blue"
+                         document.getElementById("body").style.color = "black";
+
+}
+
+function copyright() {alert("Autora: Ariadna Tabani")}
